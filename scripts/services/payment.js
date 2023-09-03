fb44e82d891afa2c1cd7aa96377117d81f7dcdf4
Sparsh Rawat <92641998+Sparsh225@users.noreply.github.com>
2023-09-03 09:46:16 +0530

    Add files via upload

---
+++ scripts/services/payment.js
@@ -0,0 +1,40 @@
+var options = {
+    "key": "rzp_test_slz0joRsja8AxE", // Enter the Key ID generated from the Dashboard
+    "amount": "50000", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+    "currency": "INR",
+    "name": "Pizza Shop", //your business name
+    "description": "pizza shop Transaction",
+    "image": "https://example.com/your_logo",
+    //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+    "handler": function (response){
+        alert("payment done");
+        alert(response.razorpay_payment_id);
+        alert(response.razorpay_order_id);
+        alert(response.razorpay_signature)
+    },
+    "prefill": { //We recommend using the prefill parameter to auto-fill customer's contact information, especially their phone number
+        "name": "Customer name", //your customer's name
+        "email": "CustomerEmail@example.com", 
+        "contact": "9000090000"  //Provide the customer's phone number for better conversion rates 
+    },
+    "notes": {
+        "address": "Razorpay Corporate Office"
+    },
+    "theme": {
+        "color": "#3399cc"
+    }
+};
+var rzp1 = new Razorpay(options);
+rzp1.on('payment.failed', function (response){
+        alert(response.error.code);
+        alert(response.error.description);
+        alert(response.error.source);
+        alert(response.error.step);
+        alert(response.error.reason);
+        alert(response.error.metadata.order_id);
+        alert(response.error.metadata.payment_id);
+});
+document.getElementById('rzp-button1').addEventListener('click',function(){
+    rzp1.open();
+    e.preventDefault();
+});
